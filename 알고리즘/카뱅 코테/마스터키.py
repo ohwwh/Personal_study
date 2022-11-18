998598612d267f7952a65f061194345d7e9791b2
ohwwh <ohw1237@gmail.com>
2022-11-18 12:09:36 +0900

    카뱅 3번 해설 달아 놓았음

--- 알고리즘/카뱅 코테/마스터키.py
+++ 알고리즘/카뱅 코테/마스터키.py
@@ -43,4 +43,21 @@ def solution(rooms):
 		unprocessed = list(map(list, combinations(idx_lst, k)))
 	return n
 
-print(solution([1,2,3,4,5,6]))
+'''def solution(rooms):
+    answer = 0
+    visited = [False for _ in range(len(rooms))]
+    for i in range(len(rooms)):
+        if visited[i] == False:
+            answer += 1
+            v = rooms[i] - 1
+            visited[i] = True
+            while (visited[v] == False):
+                visited[v] = True
+                v = rooms[v] - 1
+    if (answer != 1):
+        answer -=1
+    return answer'''
+	# 전체 방이 몇 개 그룹으로 분할되는지 확인하기만 하면 되는 문제. 너무 어렵게 생각했다
+	# 그룹이란 연쇄적으로 열릴 수 있는 방의 집합을 의미한다
+
+print(solution([10,8,1,5,3,2,9,4,7,6]))
